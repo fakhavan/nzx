7912217ec965492ff345fb36f67c4a9c6dc50368
zabenno <benc853@gmail.com>
2020-12-18 22:09:32 +1300

    Adding more data to mainboard objects.

--- nzx_scraper/nzx_mainboard_stock.py
+++ nzx_scraper/nzx_mainboard_stock.py
@@ -3,25 +3,45 @@ from  bs4 import BeautifulSoup
 class nzx_mainboard_stock():
 
     def __init__(self, stock_raw):
-        self.ticker = self.get_stock_ticker(stock_raw)
-        self.name = self.get_stock_name(stock_raw)
-        self.price = self.get_stock_price(stock_raw)
-        self.change = None
-        self.value = None
-        self.volume = None
-        self.capitalisation = None
+        self.ticker = self.get_ticker(stock_raw)
+        self.name = self.get_name(stock_raw)
+        self.price = self.get_price(stock_raw)
+        self.change = self.get_change(stock_raw)
+        self.value = self.get_value(stock_raw)
+        self.volume = self.get_volume(stock_raw)
+        self.capitalisation = self.get_capitalisation(stock_raw)
 
-    def get_stock_ticker(self, stock_raw):
+    def get_ticker(self, stock_raw):
         result = None
         result = stock_raw.find('td').find('strong', class_="").text
         return result
     
-    def get_stock_name(self, stock_raw):
+    def get_name(self, stock_raw):
         result = None
         result = stock_raw.find_all('a')[1].text
         return result
 
-    def get_stock_price(self, stock_raw):
+    def get_price(self, stock_raw):
         result = None
         result = stock_raw.select('td[data-title="Price"]')[0].text.strip()[1:]
+        return result
+    
+    def get_change(self, stock_raw):
+        result = None
+        result = stock_raw.select('td[data-title="ChangePercent"]')[0].text.strip()
+        return result
+
+    def get_value(self, stock_raw):
+        result = None
+        result = stock_raw.select('td[data-title="Value"]')[0].text.strip()[1:]
+        return result
+
+    def get_volume(self, stock_raw):
+        result = None
+        result = stock_raw.select('td[data-title="Volume"]')[0].text.strip()
+        return result
+
+    def get_capitalisation(self, stock_raw):
+        result = None
+        result = stock_raw.select('td[data-title="Capitalisation"]')[0].text.strip()
         return result
